a8b9f51545792ffcc575feb9e9f1f59646543993
nokaa <nokaa@cock.li>
2017-01-11 16:38:57 -0600

    Kernel heap

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,26 @@
+#![feature(alloc, collections)]
 #![feature(const_fn)]
 #![feature(lang_items)]
 #![feature(unique)]
 #![no_std]
 
+extern crate alloc;
 #[macro_use]
 extern crate bitflags;
+#[macro_use]
+extern crate collections;
+extern crate hole_list_allocator;
 extern crate multiboot2;
+#[macro_use]
+extern crate once;
 extern crate rlibc;
 extern crate spin;
 extern crate volatile;
 extern crate x86;
 
+mod memory;
 #[macro_use]
 mod vga_buffer;
-mod memory;
 
 #[no_mangle]
 pub extern "C" fn rust_main(multiboot_information_address: usize) {
@@ -22,49 +29,28 @@ pub extern "C" fn rust_main(multiboot_information_address: usize) {
     println!("Hello World{}", "!");
 
     let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
-    let memory_map_tag = boot_info.memory_map_tag()
-        .expect("Memory map tag required");
-    let elf_sections_tag = boot_info.elf_sections_tag()
-        .expect("Elf-sections tag required");
-
-    println!("memory areas:");
-    for area in memory_map_tag.memory_areas() {
-        println!("    start: 0x{:x}, length: 0x{:x}",
-                 area.base_addr,
-                 area.length);
-    }
-
-    println!("kernel sections:");
-    for section in elf_sections_tag.sections() {
-        println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
-                 section.addr,
-                 section.size,
-                 section.flags);
-    }
-
-    let kernel_start = elf_sections_tag.sections().map(|s| s.addr).min().unwrap();
-    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size).max().unwrap();
-
-    let multiboot_start = multiboot_information_address;
-    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
-
-    println!("kernel start: 0x{:x}, kernel end: 0x{:x}",
-             kernel_start,
-             kernel_end);
-    println!("multiboot start: 0x{:x}, multiboot end: 0x{:x}",
-             multiboot_start,
-             multiboot_end);
-
-    let mut frame_allocator = memory::AreaFrameAllocator::new(kernel_start as usize,
-                                                              kernel_end as usize,
-                                                              multiboot_start,
-                                                              multiboot_end,
-                                                              memory_map_tag.memory_areas());
-
     enable_nxe_bit();
     enable_write_protect_bit();
 
-    memory::remap_the_kernel(&mut frame_allocator, boot_info);
+    // set up guard page and map the heap pages
+    memory::init(boot_info);
+
+    use alloc::boxed::Box;
+    let mut heap_test = Box::new(42);
+    *heap_test -= 15;
+    let heap_test2 = Box::new("hello");
+    println!("{:?} {:?}", heap_test, heap_test2);
+
+    let mut vec_test = vec![1, 2, 3, 4, 5, 6, 7];
+    vec_test[3] = 42;
+    for i in &vec_test {
+        print!("{} ", i);
+    }
+
+    for i in 0..10000 {
+        format!("Some String");
+    }
+
     println!("It did not crash!");
 
     loop {}
